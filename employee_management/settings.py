f72959978c8e8b719802f372a3d18eff5905eb81
Jim <chengxuyuan9527@gmail.com>
2022-03-12 21:56:17 +1100

    create tables

--- employee_management/settings.py
+++ employee_management/settings.py
@@ -75,10 +75,18 @@ WSGI_APPLICATION = 'employee_management.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    
+       'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'employee_management',
+        'USER': "root",
+        'PASSWORD': "1122334",
+        "HOST": "127.0.0.1",
+        "PORT": 3306,
     }
+    
+    
+    
 }
 
 
